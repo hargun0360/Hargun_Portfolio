afd8354d03b956506c702ac24be936413dcc4114
hargun0360 <hargunsingh0720@gmail.com>
2023-07-30 11:08:19 +0530

    Introduce section complete

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hamburger from '../Components/Hamburger'
 import Sidebar from '../Components/Sidebar'
 import Intro from '../Components/Intro'
+import Type from '../Components/Type'
 
 const Home = () => {
   return (<>
@@ -15,8 +16,29 @@ const Home = () => {
               <section id="home" className="scroll-to-page page-section hero-section">
                   <div className="custom-container">
                     <div className="hero-content content-width">
-                      <div className="section-header"></div>
-                      <p className="scroll-animation"></p>
+                      <div className="section-header">
+                        <h4 className='subtitle scroll-animation'>
+                          <i className='las la-home'></i>
+                           Introduce
+                        </h4>
+                        <h2 className="scroll-animation">
+                          Hi There!{" "} 
+                          <span className="wave" role="img" aria-labelledby="wave">
+                            👋🏻
+                          </span>
+                        </h2>
+                        <h1 className="heading-name">
+                          I'M
+                          <strong className="main-name"> Hargun Singh</strong>
+                        </h1>
+                        <h1 className="type-writter-effect">
+                         <Type />
+                        </h1>
+                      </div>
+                      <p className="para scroll-animation">
+                       I am an enthusiastic learner who thrives on acquiring new skills and applying them in real-life projects,
+                       constantly evolving to create innovative and impactful solutions.
+                      </p>
                     </div>
                   </div>
               </section>
