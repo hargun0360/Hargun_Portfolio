441663f446bf967e6548d12d758902c49b8cec77
hargun0360 <hargunsingh0720@gmail.com>
2023-08-01 09:05:21 +0530

    Add Coding Profiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import './Styles/main.scss';
-import video from './Assets/video3.mp4';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Experience from './Pages/Experience';
-import Project from './Pages/Project';
-import Skills from './Pages/Skills';
+import React from "react";
+import "./Styles/main.scss";
+import video from "./Assets/video3.mp4";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Experience from "./Pages/Experience";
+import Project from "./Pages/Project";
+import Skills from "./Pages/Skills";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      <video className='body-overlay' muted autoPlay loop>
+      <video className="body-overlay" muted autoPlay loop>
         <source src={video}></source>
       </video>
       <Home />
@@ -18,6 +19,19 @@ function App() {
       <Experience />
       <Project />
       <Skills />
+      <Profile />
+      <section
+        className="project-area page-section scroll-to-page"
+        id="project"
+      >
+        <div className="custom-container">
+          <div className="prject-content content-width">
+            <div className="footer">
+          <h3>Designed by Drake And Developed By Hargun Singh With Love ❤️</h3>
+          </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/Pages/Profile.tsx
@@ -0,0 +1,37 @@
+
+import leetcode from "../Assets/icons8-level-up-your-coding-skills-and-quickly-land-a-job-96 (1).png"
+import codeforces from "../Assets/icons8-codeforces.-programming-competitions-and-contests,-programming-community.-96.png"
+import codechef from "../Assets/codechef.png"
+
+function Profile() {
+
+  return (
+    <>
+      <section
+        className="project-area page-section scroll-to-page"
+        id="project"
+      >
+        <div className="custom-container">
+          <div className="prject-content content-width">
+            <div className="section-header">
+              <h4 className="subtitle scroll-animation">
+              <i className='las la-code'></i>
+                Coding Profiles
+              </h4>
+            </div>
+            <div className="coding-profile">
+                <h1>Accomplished <span>500+</span> Data Structure and Algorithm Problems on Multiple Platforms.</h1>
+                <div className="links">
+                    <img src={leetcode} alt="leetcode" />
+                    <img src={codeforces} alt="codeforces" />
+                    <img src={codechef} className="codechef" alt="codechef" />
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Profile;
