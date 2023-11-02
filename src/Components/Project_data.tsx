baadf591e98a9035db727cc77c5d2d5a0dfb5f30
hargun0360 <hargunsingh0720@gmail.com>
2023-11-02 06:38:14 +0530

    Chnage: Link for ERP Project

--- src/Components/Project_data.tsx
+++ src/Components/Project_data.tsx
@@ -22,24 +22,6 @@ export default function projects_data() {
     },
 
     {
-      heading: "Digitized Queue Management System",
-      description: `Developed a React app with Machine Learning to virtualize store queues, enhancing customer experience. Used ARIMA for precise wait-time forecasting.
-            `,
-      tech: [
-        "React",
-        "Redux",
-        "Node",
-        "JavaScript",
-        "Express",
-        "MongoDB",
-        "Rest API",
-      ],
-      img: wait,
-      github: "https://github.com/hargun0360/QueueManagement",
-      link: "https://saksham-tiwari.github.io/QueueManagement",
-    },
-
-    {
       heading: "College Management System",
       description: `Lead ERP development for college using React. Created Admin, Faculty, and Student panels.
             Used Redux Thunk, Chart.JS, and Material UI, Implemented access and refresh token logic.`,
@@ -56,7 +38,7 @@ export default function projects_data() {
       ],
       img: Landing,
       github: "https://github.com/hargun0360/College-ERP",
-      link: "https://college-erp-e09c2.web.app/",
+      link: "https://akgec-erp.netlify.app/",
     },
 
     {
@@ -92,6 +74,23 @@ export default function projects_data() {
       img: openinapp,
       github: "https://github.com/hargun0360/OpeninApp_React_Task",
       link: "https://openinapp-assignment-hargun-singh.netlify.app/",
+    },
+    {
+      heading: "Digitized Queue Management System",
+      description: `Developed a React app with Machine Learning to virtualize store queues, enhancing customer experience. Used ARIMA for precise wait-time forecasting.
+            `,
+      tech: [
+        "React",
+        "Redux",
+        "Node",
+        "JavaScript",
+        "Express",
+        "MongoDB",
+        "Rest API",
+      ],
+      img: wait,
+      github: "https://github.com/hargun0360/QueueManagement",
+      link: "https://saksham-tiwari.github.io/QueueManagement",
     },
   ];
 
